5563e93e90a07353cb4640f82e489362b2318676
AliDaniel1 <alidanielh123@gmail.com>
2022-06-16 11:05:59 -0430

    Commit#2(estilos y estructura)

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 
-var tablatxt = document.getElementById("number");
+var Tabla_Info = document.getElementById("number");
+var Tm = document.getElementById("TABLA"); 
 
 function multiplica(a,b){
 	return a * b
 };
 
 let tabla = prompt("¿Que tabla de multiplicar deseas aprender?");
-tablatxt.innerHTML = "Tabla de multiplicar del numero " + tabla;
+Tabla_Info.innerHTML = `Tabla de multiplicar del numero ${tabla}`;
 
-if(tabla > 0){
+if(tabla > 0||tabla < 0){
 	for(let i = 1 ; i <= 10;i++){
 		var op = multiplica(i,tabla);
-		document.write("<h3>" + tabla + " x " + i +" = " + op + "<br>");
-};
+		Tm.innerHTML += `${tabla} x ${i} = ${op}<br>`;
+	};
 }else if(tabla==""||tabla == null){
-	alert("Debe ingresar un valor")
-	location.reload();
+	alert("Debe ingresar un valor");
 }else{
 	alert("Esta tabla no existe");
-	tablatxt.innerHTML = "la tabla que ingreso no existe";
+	Tabla_Info.innerHTML = "la tabla que ingreso no existe";
 }
