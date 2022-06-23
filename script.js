a7e125a3d5941f72f620501014e53e9ba8e8b965
AliDaniel1 <alidanielh123@gmail.com>
2022-06-23 13:54:02 -0430

    Commit#4(Cambios en Js)

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-
 var Tabla_number = document.getElementById("Tabla_number");
-var Tabla_Info = document.getElementById("number");
-var Tm = document.getElementById("TABLA"); 
+var Tabla_Info = document.getElementById("Tabla_Info");
+var Tm = document.getElementById("TABLA");
 
-multiplica = (a,b) => {
+multiplica = (a,b)=>{
 	return a * b
 };
 
-const Crear_Tabla = () => {
+const Crear_Tabla = ()=>{
 
 Tabla_Info.innerHTML = `Tabla de multiplicar del numero ${Tabla_number.value}`;
 
 if(Tabla_number.value > 0||Tabla_number.value < 0){
-	for(let i = 1 ; i <= 10;i++){
+	for(let i = 1 ;i <= 10 ;i++){
 		var op = multiplica(i,Tabla_number.value);
 		Tm.innerHTML += `${Tabla_number.value} x ${i} = ${op}<br>`;
 	};
 }else if(Tabla_number.value == ""||Tabla_number.value == null){
 	alert("Debe ingresar un valor");
+	Tabla_Info.innerHTML = "Debe ingresar un numero";
+	Tabla_Info.style.color = "#000"
 }else{
 	alert("Esta tabla no existe");
 	Tabla_Info.innerHTML = "la tabla que ingreso no existe";
-}
+	Tabla_Info.style.color = "#f00"
+};
+};
+
+const Reiniciar = ()=>{
+	Tabla_Info.innerHTML = "";
+	Tm.innerHTML = "";
 }
