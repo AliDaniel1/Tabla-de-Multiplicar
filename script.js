5956b0608854df194b52319cd141d7e44f70ef07
AliDaniel1 <alidanielh123@gmail.com>
2022-07-03 12:50:29 -0430

    Commit#6(Pequeños ajustes)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var Tabla_number = document.getElementById("Tabla_number");
-var Tabla_Info = document.getElementById("Tabla_Info");
-var TABLA = document.getElementById("TABLA");
+var Text_Info = document.getElementById("Text_Info");
+var Tabla = document.getElementById("Tabla");
 
 const multiplicar = (a,b)=>{
 	return a * b
@@ -8,34 +8,46 @@ const multiplicar = (a,b)=>{
 
 const Crear_Tabla = ()=>{
 
-Tabla_Info.innerHTML = `Tabla de multiplicar del numero ${Tabla_number.value}`;
+Text_Info.innerHTML = `Tabla de multiplicar del numero ${Tabla_number.value}`;
 
 if(Tabla_number.value > 0||Tabla_number.value < 0){
 	for(let i = 1 ;i <= 10 ;i++){
 		var op = multiplicar(i,Tabla_number.value);
-		TABLA.innerHTML += `${Tabla_number.value} x ${i} = ${op}<br>`;
-		Tabla_Info.style.color = "#3793F0"
+		Tabla.innerHTML += `${Tabla_number.value} x ${i} = ${op}<br>`;
+		Text_Info.style.color = "#3793F0"
 	};
 }else if(Tabla_number.value == ""||Tabla_number.value == null){
 	alert("Debe ingresar un valor");
-	Tabla_Info.innerHTML = "Debe ingresar un numero";
-	Tabla_Info.style.color = "#000"
+	Text_Info.innerHTML = "Debe ingresar un numero";
+	Text_Info.style.color = "#000"
 }else{
 	alert("Esta tabla no existe");
-	Tabla_Info.innerHTML = "La tabla que ingreso no existe,intente con otro valor";
-	Tabla_Info.style.color = "#ff3f3f"
+	Text_Info.innerHTML = "La tabla que ingreso no existe,intente con otro valor";
+	Text_Info.style.color = "#ff3f3f"
 };
 };
 
 const Reiniciar = ()=>{
 	Tabla_number.value = "";
-	Tabla_Info.innerHTML = "";
-	TABLA.innerHTML = "";
+	Text_Info.innerHTML = "";
+	Tabla.innerHTML = "";
 }
 
 document.addEventListener("keydown" , (e)=>{
-	const tecla = event.key;
-	if(tecla === 'Backspace'){
+	const tecla = e.keyCode;
+	if(tecla == 8){
+		if(Tabla_number.value !== null){
+			Text_Info.innerHTML = "";
+			Tabla.innerHTML = "";
+		}else{
 		return Reiniciar();
+		};
+	};
+})
+
+document.addEventListener("keydown" , (e)=>{
+	const tecla = e.keyCode;
+	if(tecla == 13){
+		e.preventDefault();
 	};
 })
