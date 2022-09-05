3eb8260ff605be749fda034c5f6834bff270ea91
Ali Hammoud <alidanielh123@gmail.com>
2022-09-04 20:39:29 -0400

    Ajustes y mejoras

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 var Tabla_number = document.getElementById("Tabla_number");
 var Text_Info = document.getElementById("Text_Info");
 var Tabla = document.getElementById("Tabla");
+var Selector_Fila = document.getElementById("Selector_fila");
 
 const multiplicar = (a,b)=>{
 	return a * b
 };
 
-const Crear_Tabla = ()=>{
+const Crear_Tabla = (n)=>{
 
 Text_Info.innerHTML = `Tabla de multiplicar del numero ${Tabla_number.value}`;
 
 if(Tabla_number.value > 0||Tabla_number.value < 0){
-	for(let i = 1 ;i <= 10 ;i++){
+	for(let i = 1 ;i <= n ;i++){
 		var op = multiplicar(i,Tabla_number.value);
-		Tabla.innerHTML += `${Tabla_number.value} x ${i} = ${op}<br>`;
+		Tabla.innerHTML += `${Tabla_number.value} x ${i} = ${Math.round(op*100)/100}<br>`;
 		Text_Info.style.color = "#3793F0"
 	};
 }else if(Tabla_number.value == ""||Tabla_number.value == null){
@@ -28,20 +29,14 @@ if(Tabla_number.value > 0||Tabla_number.value < 0){
 };
 
 const Reiniciar = ()=>{
-	Tabla_number.value = "";
 	Text_Info.innerHTML = "";
 	Tabla.innerHTML = "";
-}
+};
 
 document.addEventListener("keydown" , (e)=>{
 	const tecla = e.keyCode;
 	if(tecla == 8){
-		if(Tabla_number.value !== null){
-			Text_Info.innerHTML = "";
-			Tabla.innerHTML = "";
-		}else{
 		return Reiniciar();
-		};
 	};
 })
 
@@ -50,4 +45,25 @@ document.addEventListener("keydown" , (e)=>{
 	if(tecla == 13){
 		e.preventDefault();
 	};
+})
+const Seleccionar_filas = ()=>{
+	if(Selector_Fila.value =="1"){
+		Reiniciar();
+		Crear_Tabla(10);
+	}if(Selector_Fila.value =="2"){
+		Reiniciar();
+		Crear_Tabla(20);
+	}if(Selector_Fila.value =="3"){
+		Reiniciar();
+		Crear_Tabla(50);
+	}if(Selector_Fila.value =="4"){
+		Reiniciar();
+		Crear_Tabla(100);
+	}if(Selector_Fila.value =="5"){
+		Reiniciar();
+		Crear_Tabla(200);
+	};
+};
+document.getElementById("Btn").addEventListener("click" , ()=>{
+	Seleccionar_filas();
 })
